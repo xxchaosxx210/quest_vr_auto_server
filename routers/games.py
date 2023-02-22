30120e1557b2603ce073202fae4b5e707172d613
xxchaosxx210 <pmillar1279@googlemail.com>
2023-02-22 10:33:04 +0000

    get_games now returns a list
    added some comments to the code as its starting to get a little larger

--- routers/games.py
+++ routers/games.py
@@ -9,16 +9,24 @@ from routers.users import get_current_active_admin, get_current_user
 router = fastapi.APIRouter(prefix="/games", tags=["Games"])
 
 
-@router.get("", status_code=fastapi.status.HTTP_200_OK)
-async def get_games() -> dict:
-    # game_models = db.query(models.Games).all()
+@router.get(
+    "", status_code=fastapi.status.HTTP_200_OK, response_model=List[schemas.Game]
+)
+async def get_games():
+    """retrieves all the game magnets found in the database
+    no authentication is required for now. May change at a later date
 
+    Response:
+        List[schemas.Game]
+    """
+
+    # game_models = db.query(models.Games).all()
     def get_game_schema(game: dict):
         return schemas.Game(**game)
 
     response = base_games.fetch()
     games = list(map(get_game_schema, response.items))
-    return {"games": games}
+    return games
 
 
 @router.get(
@@ -30,6 +38,18 @@ async def find_games(
     search_request: schemas.SearchGameRequest,
     current_user: schemas.User = fastapi.Depends(get_current_user),
 ):
+    """searches the database for games using the search json query
+
+    Args:
+        search_request (schemas.SearchGameRequest):
+        current_user (schemas.User, optional): _description_. Defaults to fastapi.Depends(get_current_user).
+
+    Raises:
+        fastapi.HTTPException: 404 not found error
+
+    Returns:
+        List[GameWithKey]:
+    """
     query_req = search_request.dict(exclude_unset=True)
     query_resp = base_games.fetch(query_req)
     if query_resp.count == 0:
