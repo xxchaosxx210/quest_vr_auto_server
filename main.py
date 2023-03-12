559386afd00cb8ef4af17014ca1b9e8f6c640e51
xxchaosxx210 <pmillar1279@googlemail.com>
2023-03-12 10:44:36 +0000

    using the template method

--- main.py
+++ main.py
@@ -3,11 +3,17 @@
 
 import fastapi
 
+# from fastapi.staticfiles import StaticFiles
+# from fastapi.routing import Mount
+# from fastapi.templating import Jinja2Templates
+
 from routers import games
 from routers import logs
 from routers import users
 from routers import help
 
+
+# routes = [Mount("/static", StaticFiles(directory="static"), name="static")]
 
 app = fastapi.FastAPI()
 
