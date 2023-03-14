66a4fb60e87398bcf23a5d31cf9d20600bfb2ce2
xxchaosxx210 <pmillar1279@googlemail.com>
2023-03-14 04:44:00 +0000

    updated

--- routers/site.py
+++ routers/site.py
@@ -21,6 +21,42 @@ def get_index(request: Request):
     response_class=HTMLResponse,
     status_code=fastapi.status.HTTP_200_OK,
 )
-async def get_help(request: Request):
+async def get_help_contents(request: Request):
     context = config.get_context(request)
-    return config.templates.TemplateResponse("help.html", context)
+    return config.templates.TemplateResponse("help-contents.html", context)
+
+
+@router.get(
+    "/help/quest-setup",
+    response_class=HTMLResponse,
+    status_code=fastapi.status.HTTP_200_OK,
+)
+async def get_help_quest_setup(request: Request):
+    context = config.get_context(request)
+    return config.templates.TemplateResponse("help-part1.html", context)
+
+
+@router.get(
+    "/help/quest-cave-guide",
+    response_class=HTMLResponse,
+    status_code=fastapi.status.HTTP_200_OK,
+)
+async def get_help_questcave_guide(request: Request):
+    context = config.get_context(request)
+    return config.templates.TemplateResponse("help-part2.html", context)
+
+
+@router.get(
+    "/download", status_code=fastapi.status.HTTP_200_OK, response_class=HTMLResponse
+)
+async def get_download(request: Request):
+    context = config.get_context(request)
+    return config.templates.TemplateResponse("download.html", context)
+
+
+@router.get(
+    "/contact", status_code=fastapi.status.HTTP_200_OK, response_class=HTMLResponse
+)
+async def get_contact(request: Request):
+    context = config.get_context(request)
+    return config.templates.TemplateResponse("contact.html", context)
